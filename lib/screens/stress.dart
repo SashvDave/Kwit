f94c270a9384fa5ffefa268a7edeb37234ebb123
Sashv Dave <davesashv@gmail.com>
2020-12-19 19:08:44 -0800

    Add in navigation

--- lib/screens/stress.dart
+++ lib/screens/stress.dart
@@ -80,7 +80,7 @@ class _StressState extends State<Stress> {
                                 ),
                                 child: Container(
                                   height:
-                                      MediaQuery.of(context).size.height * 0.2,
+                                      MediaQuery.of(context).size.height * 0.17,
                                   padding: EdgeInsets.only(
                                       top: 20, left: 20, right: 20),
                                   child: Text(
@@ -104,7 +104,10 @@ class _StressState extends State<Stress> {
                               width: 520,
                               child: Card(
                                 margin: EdgeInsets.only(
-                                    top: MediaQuery.of(context).size.height * 0.05, right: 30, left: 30),
+                                    top: MediaQuery.of(context).size.height *
+                                        0.008,
+                                    right: 30,
+                                    left: 30),
                                 semanticContainer: true,
                                 clipBehavior: Clip.antiAliasWithSaveLayer,
                                 child: Image.asset(
@@ -118,40 +121,42 @@ class _StressState extends State<Stress> {
                               ),
                             ),
                             Container(
-                              decoration: BoxDecoration(
-                                color: const Color(0xFFFFFFF),
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                    'assets/happy.png',),
-                                  fit: BoxFit.cover,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFFFFFFF),
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                      'assets/happy.png',
+                                    ),
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
-                              ),
-                              child: Container(
-                                width: 750,
-                                child: IconButton(
-                                padding: EdgeInsets.only(
-                                    top: 55, bottom: 15),
-                                iconSize: 100,
-                                icon: Icon(
-                                  isPlaying ? Icons.pause : Icons.play_arrow,
-                                  color: const Color(0xFF4FCB79),
-                                ),
-                                onPressed: () {
-                                  if (isPlaying) {
-                                    advancedPlayer.pause();
-                                    setState(() {
-                                      isPlaying = false;
-                                    });
-                                  } else {
-                                    audioCache.play('Calm.mp3');
-                                    setState(() {
-                                      isPlaying = true;
-                                    });
-                                  }
-                                },
-                              ),
-                              )
-                            )
+                                child: Container(
+                                  width: 750,
+                                  child: IconButton(
+                                    padding:
+                                        EdgeInsets.only(top: 25, bottom: 15),
+                                    iconSize: 100,
+                                    icon: Icon(
+                                      isPlaying
+                                          ? Icons.pause
+                                          : Icons.play_arrow,
+                                      color: const Color(0xFF4FCB79),
+                                    ),
+                                    onPressed: () {
+                                      if (isPlaying) {
+                                        advancedPlayer.pause();
+                                        setState(() {
+                                          isPlaying = false;
+                                        });
+                                      } else {
+                                        audioCache.play('Calm.mp3');
+                                        setState(() {
+                                          isPlaying = true;
+                                        });
+                                      }
+                                    },
+                                  ),
+                                ))
                           ],
                         ),
                         shape: RoundedRectangleBorder(
