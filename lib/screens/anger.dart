6ba40f0d6f326f37487a0d9d5ec59589982bcd7f
Ved Joshi <50056400+Ved-Joshi@users.noreply.github.com>
2020-12-27 21:43:24 -0800

    fix anger therapy margins

--- lib/screens/anger.dart
+++ lib/screens/anger.dart
@@ -49,133 +49,104 @@ class _AngerState extends State<Anger> {
                   child: Icon(Icons.portrait_rounded)))
         ],
       ),
-      body: Stack(children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/bg6.png'),
-              fit: BoxFit.cover,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/bg6.png'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                  margin: EdgeInsets.only(left: 35, right: 35, top: 10),
-                  child: new InkWell(
-                    child: Card(
-                      semanticContainer: true,
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      child: Column(
-                        children: [
-                          Container(
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage("assets/happy.png"),
-                                  fit: BoxFit.fitWidth,
-                                  alignment: Alignment.topCenter,
-                                ),
-                              ),
-                              child: Container(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.17,
-                                padding: EdgeInsets.only(
-                                    top: 20, left: 20, right: 20),
-                                child: Text(
-                                  "Play the music to start your therapy",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 18),
-                                ),
-                              )),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: const Color(0xFFFFFFF),
-                              image: DecorationImage(
-                                image: AssetImage('assets/happy.png'),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            height: 220,
-                            width: 520,
-                            child: Card(
-                              margin: EdgeInsets.only(right: 30, left: 30),
-                              semanticContainer: true,
-                              clipBehavior: Clip.antiAliasWithSaveLayer,
-                              child: Image.asset(
-                                'assets/sunset.png',
-                                fit: BoxFit.fill,
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              elevation: 5,
-                            ),
-                          ),
-                          Container(
-                              decoration: BoxDecoration(
-                                color: const Color(0xFFFFFFF),
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                    'assets/happy.png',
-                                  ),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              child: Container(
-                                width: 750,
-                                child: IconButton(
-                                  padding: EdgeInsets.only(top: 25, bottom: 15),
-                                  iconSize: 100,
-                                  icon: Icon(
-                                    isPlaying ? Icons.pause : Icons.play_arrow,
-                                    color: const Color(0xFF4FCB79),
-                                  ),
-                                  onPressed: () {
-                                    if (isPlaying) {
-                                      advancedPlayer.pause();
-                                      setState(() {
-                                        isPlaying = false;
-                                      });
-                                    } else {
-                                      audioCache.play('Calm.mp3');
-                                      setState(() {
-                                        isPlaying = true;
-                                      });
-                                    }
-                                  },
-                                ),
-                              ))
-                        ],
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      elevation: 10,
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Transform.scale(
+                  scale: 2.5,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green[100],
+                      shape: BoxShape.circle,
                     ),
-                  )),
-              new Container(
-                margin: EdgeInsets.only(top: 10, left: 10),
-                width: 400,
-                padding: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () => _submitData(),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                  child: Text('Next',
-                      style: TextStyle(color: Colors.black, fontSize: 17)),
-                  padding: const EdgeInsets.all(13.0),
-                  splashColor: Colors.lightBlue[200],
-                  color: const Color(0xFFB8ECF0),
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(
+                              0,
+                              MediaQuery.of(context).size.height * 0.15,
+                              0,
+                              MediaQuery.of(context).size.height * 0.015),
+                          child: Text(
+                            "Play the music to start your therapy",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 7),
+                          ),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: Image.asset(
+                            'assets/sunset.png',
+                            height: MediaQuery.of(context).size.height * 0.14,
+                            width: MediaQuery.of(context).size.width * 0.37,
+                          ),
+                        ),
+                        Container(
+                          width: 750,
+                          child: IconButton(
+                            padding: EdgeInsets.only(top: 15, bottom: 10),
+                            iconSize: 30,
+                            icon: Icon(
+                              isPlaying ? Icons.pause : Icons.play_arrow,
+                              color: const Color(0xFF1976D2),
+                            ),
+                            onPressed: () {
+                              if (isPlaying) {
+                                advancedPlayer.pause();
+                                setState(() {
+                                  isPlaying = false;
+                                });
+                              } else {
+                                audioCache.play('Calm.mp3');
+                                setState(() {
+                                  isPlaying = true;
+                                });
+                              }
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        )
-      ]),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.02,
+                  left: MediaQuery.of(context).size.width * 0.01),
+              width: MediaQuery.of(context).size.width * 0.98,
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () => _submitData(),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                child: Text('Next',
+                    style: TextStyle(color: Colors.black, fontSize: 17)),
+                padding: const EdgeInsets.all(13.0),
+                splashColor: Colors.lightBlue[200],
+                color: const Color(0xFFB8ECF0),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/anger3.dart
+++ lib/screens/anger3.dart
@@ -44,126 +44,130 @@ class _Anger3State extends State<Anger3> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Kwit: Anger Relief'),
-        actions: [
-          Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Anger1()));
-                  },
-                  child: Icon(Icons.portrait_rounded)))
-        ],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-              'assets/bg9.png',
-            ),
-            fit: BoxFit.cover,
-          ),
+        appBar: AppBar(
+          title: Text('Kwit: Anger Relief'),
+          actions: [
+            Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Anger1()));
+                    },
+                    child: Icon(Icons.portrait_rounded)))
+          ],
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircularCountDownTimer(
-                width: MediaQuery.of(context).size.width * 0.2,
-                height: MediaQuery.of(context).size.width * 0.2,
-                duration: 75,
-                fillColor: Colors.white,
-                color: Colors.greenAccent,
-                textStyle: TextStyle(
-                  color: Colors.amber,
-                  fontWeight: FontWeight.bold,
-                ),
-                isReverse: true,
-                isReverseAnimation: true,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text(
-                  "Draw your emotions. Upload an image to the app.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/bg9.png',
+                  ),
+                  fit: BoxFit.cover,
                 ),
               ),
-              _imageFile != null
-                  ? ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Container(
-                        color: Colors.blue[100],
-                        child: SizedBox(
-                          child: Image.file(_imageFile),
-                          height: MediaQuery.of(context).size.height * 0.4,
-                          width: MediaQuery.of(context).size.width * 0.9,
-                        ),
-                      ),
-                    )
-                  : ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Container(
-                        color: Colors.blue[100],
-                        child: SizedBox(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.camera_alt,
-                                size: 50,
-                              ),
-                              Text("No image uploaded")
-                            ],
+            ),
+            SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularCountDownTimer(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    height: MediaQuery.of(context).size.width * 0.2,
+                    duration: 75,
+                    fillColor: Colors.white,
+                    color: Colors.greenAccent,
+                    textStyle: TextStyle(
+                      color: Colors.amber,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    isReverse: true,
+                    isReverseAnimation: true,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                      "Draw your emotions. Upload an image to the app.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18),
+                    ),
+                  ),
+                  _imageFile != null
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: Container(
+                            color: Colors.blue[100],
+                            child: SizedBox(
+                              child: Image.file(_imageFile),
+                              height: MediaQuery.of(context).size.height * 0.4,
+                              width: MediaQuery.of(context).size.width * 0.9,
+                            ),
                           ),
-                          height: MediaQuery.of(context).size.height * 0.4,
-                          width: MediaQuery.of(context).size.width * 0.9,
+                        )
+                      : ClipRRect(
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: Container(
+                            color: Colors.blue[100],
+                            child: SizedBox(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.camera_alt,
+                                    size: 50,
+                                  ),
+                                  Text("No image uploaded")
+                                ],
+                              ),
+                              height: MediaQuery.of(context).size.height * 0.4,
+                              width: MediaQuery.of(context).size.width * 0.9,
+                            ),
+                          ),
                         ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 30.0, 0, 5.0),
+                    child: RaisedButton(
+                      onPressed: () => _pickImage(),
+                      padding: EdgeInsets.all(10.0),
+                      textColor: Colors.black,
+                      splashColor: Colors.lightBlue[200],
+                      color: const Color(0xFFB8ECF0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: BorderSide(color: Colors.blue[50])),
+                      child: Text(
+                        'Upload image',
+                        style: TextStyle(fontSize: 20),
                       ),
                     ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 30.0, 0, 5.0),
-                child: RaisedButton(
-                  onPressed: () => _pickImage(),
-                  padding: EdgeInsets.all(10.0),
-                  textColor: Colors.black,
-                  splashColor: Colors.lightBlue[200],
-                  color: const Color(0xFFB8ECF0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      side: BorderSide(color: Colors.blue[50])),
-                  child: Text(
-                    'Upload image',
-                    style: TextStyle(fontSize: 20),
                   ),
-                ),
+                  new Container(
+                    margin: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width * 0.01,
+                        right: MediaQuery.of(context).size.width * 0.01),
+                    width: MediaQuery.of(context).size.width * 0.98,
+                    padding: EdgeInsets.all(10.0),
+                    child: RaisedButton(
+                      onPressed: () => _submitData(),
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(30.0))),
+                      child: Text('Next',
+                          style: TextStyle(color: Colors.black, fontSize: 17)),
+                      padding: const EdgeInsets.all(13.0),
+                      splashColor: Colors.lightBlue[200],
+                      color: const Color(0xFFB8ECF0),
+                    ),
+                  ),
+                ],
               ),
-              new Container(
-                margin: EdgeInsets.only(
-                    left: MediaQuery.of(context).size.width * 0.01,
-                    right: MediaQuery.of(context).size.width * 0.01),
-                width: MediaQuery.of(context).size.width * 0.98,
-                padding: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () => _submitData(),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                  child: Text('Next',
-                      style: TextStyle(color: Colors.black, fontSize: 17)),
-                  padding: const EdgeInsets.all(13.0),
-                  splashColor: Colors.lightBlue[200],
-                  color: const Color(0xFFB8ECF0),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          ],
+        ));
   }
 }
